5dc940c276fdb095c640dbca425eea75e5f7de9f
JBrocklyAnderson <140027721+JBrocklyAnderson@users.noreply.github.com>
2023-09-19 19:26:25 -0700

    Add Day63

---
+++ 100Days/Day63/README.md
@@ -0,0 +1,3 @@
+# Superroutines
+
+This program demonstrates my ability to create a function library from which I can pull into other files for easy execution. 
---
+++ 100Days/Day63/Superroutines.py
@@ -0,0 +1,7 @@
+import time
+import superroutines as spr ## importing `superroutines.py`
+
+spr.printColor("Welcome! This program demonstrates the ability to import functions from other files.", "green") ## calling the `printColor` function
+time.sleep(5)
+spr.clearScreen() ## calling the `clearScreen` function
+
