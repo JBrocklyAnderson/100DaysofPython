8ee7fb666ea13d2932c37ee0783a6a6feba6bf1b
JBrocklyAnderson <140027721+JBrocklyAnderson@users.noreply.github.com>
2023-09-19 19:28:51 -0700

    Update Day63

--- 100Days/Day63/Superroutines.py
+++ 100Days/Day63/Superroutines.py
@@ -1,7 +1,21 @@
-import time
-import superroutines as spr ## importing `superroutines.py`
+import os, time, random
 
-spr.printColor("Welcome! This program demonstrates the ability to import functions from other files.", "green") ## calling the `printColor` function
-time.sleep(5)
-spr.clearScreen() ## calling the `clearScreen` function
+# screen-clearing function
+def clearScreen():
+  os.system("cls" if os.name == "nt" else "clear") ## clear screen on Windows ("nt") with "cls" or Linux/MacOS with "clear"
 
+# Dictionary to map color names to their respective ANSI codes
+def printColor(word, color):
+  colors = {
+      "reset": "\033[0m",
+      "black": "\033[30m",
+      "red": "\033[31m",
+      "green": "\033[32m",
+      "yellow": "\033[33m",
+      "blue": "\033[34m",
+      "purple": "\033[35m",
+      "cyan": "\033[36m",
+      "white": "\033[37m"
+  }
+  return print(f"{colors[color]}{word}{colors['reset']}")
+  ## printColor("This is red", "red")
