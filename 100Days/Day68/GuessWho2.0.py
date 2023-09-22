9abc3514630b7d6ac450cfb5b81c27a7c7071326
JBrocklyAnderson <140027721+JBrocklyAnderson@users.noreply.github.com>
2023-09-22 00:47:33 -0700

    Add Day68

---
+++ 100Days/Day68/GuessWho2.0.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+# define image-finding subroutine
+def showImage():
+  person = text.get("1.0", "end") ## get the text from the first character (0-indexed) and the first line (indexed with 1) all the way until the end 
+  if person.strip().lower() == "charlotte":
+    canvas.itemconfig(container, image = charlotte)
+  elif person.strip().lower() == "gerald":
+    canvas.itemconfig(container, image = gerald)
+  elif person.strip().lower() == "katie":
+    canvas.itemconfig(container, image = katie)
+  elif person.strip().lower() == "mo":
+    canvas.itemconfig(container, image = mo)
+  else:
+    canvas.pack_forget() ## unpack the canvas if no image was found
+    error.pack() ## display the error message
+    return
+  error.pack_forget() ## unpack the error message
+  canvas.pack() ## only pack the canvas if an image was found
+
+# open, title, and set the size of a window
+window = tk.Tk()
+window.title("Guess Who?") 
+window.geometry("400x400") 
+
+# add a label and pack it into the window
+label = "Guess who?"
+hello = tk.Label(text = label) 
+hello.pack() 
+
+# add and pack a text box to the window
+text = tk.Text(window, height = 1, width = 30)
+text.pack()
+
+# add an error message if the image cannot be triangulated
+error = tk.Label(text = "Triangulation failed. Try again.")
+
+# add a button to initiate the image-finding subroutine
+button = tk.Button(text = "Triangulate", command = showImage)
+button.pack()
+
+# create a canvas to the window
+canvas = tk.Canvas(window, width = 300, height=300) 
+
+# load in images
+charlotte = ImageTk.PhotoImage(Image.open("GuessWho/charlotte.jpg"))
+gerald = ImageTk.PhotoImage(Image.open("GuessWho/gerald.jpg"))
+katie = ImageTk.PhotoImage(Image.open("GuessWho/katie.jpg"))
+mo = ImageTk.PhotoImage(Image.open("GuessWho/mo.jpg"))
+
+# place images in canvas
+container = canvas.create_image(200, 0, image = charlotte)
+
+# run the main loop
+tk.mainloop()
---
+++ 100Days/Day68/README.md
@@ -0,0 +1,5 @@
+# Guess Who 2.0
+
+This is a direct extension of a GUI built on Day 68 using Python's `tkinter` module. It displays and hides images and error messages based on user input into the text box. The names that a user can pull images for are "Charlotte", "Gerald", "Katie", and "Mo".  
+
+NOTE: I also updated the directory hierarchy to reduce redundancy.
